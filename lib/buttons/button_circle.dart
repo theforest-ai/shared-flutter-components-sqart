97fd28f0dfe91648f27d17162b83c9bf9e95a766
Mohammad Rendra Sura Aditama <61033485+abuabdillatief@users.noreply.github.com>
2023-03-23 09:19:18 +0700

    update package name

--- lib/buttons/button_circle.dart
+++ lib/buttons/button_circle.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:shared-flutter-components-sqart/constants/color.constant.dart';
+import 'package:shared_flutter_components_sqart/constants/color.constant.dart';
 
 class CircleButton extends StatelessWidget {
   final GestureTapCallback? onPressed;
